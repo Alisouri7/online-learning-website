75bb7b30a96e022767057bf40692f21a016d80ec
Ali Souri <souri7205@gmail.com>
2025-11-28 09:02:01 +0330

    edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 (async() => {
     await mongoose.connect(process.env.MONGO_URI)
