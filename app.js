94953ef063f5b9797365a5c72569d68e35d7e683
Ali Souri <souri7205@gmail.com>
2025-11-28 21:58:59 +0330

    init middlewares

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const express = require('express')
-const app = express()
-module.exports = app
+const express = require('express');
+
+const cors = require('cors')
+const path = require('path')
+const authRouter = require('./routes/auth');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/courses/covers', express.static(path.join(__dirname, 'courses', 'covers')));
+
+app.use('/auth', authRouter);
+
+
+module.exports = app;
