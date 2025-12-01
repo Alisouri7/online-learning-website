771371dbc4c1612e8de9225ab006f9ce619bc963
Ali Souri <souri7205@gmail.com>
2025-12-01 11:26:00 +0330

    edit static address

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use('/courses/covers', express.static(path.join(__dirname, 'courses', 'covers')));
+app.use('/courses/covers', express.static(path.join(__dirname,'public' , 'courses', 'covers')));
 
 app.use('/auth', authRouter);
 
