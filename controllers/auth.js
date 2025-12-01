ca87baf50f479e48cfa33795b4f097c0b2665977
Ali Souri <souri7205@gmail.com>
2025-12-01 12:13:51 +0330

    implent registration mechanism

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const userModel = require('./../models/user');
+const bcrypt = require('bcrypt')
 
 const registerValidator = require('./../validators/register')
 
@@ -14,8 +15,20 @@ exports.register = async (req, res) => {
         $or: [{username}, {email}]
     });
     if (isUserExist) {
-        return res.status(409).json({message: 'userame or email ha used before!'})
+        return res.status(409).json({message: 'userame or email ha used before!'});
     }
+    const countOfUsers = await userModel.countDocuments({})
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await userModel.create({
+        email,
+        username,
+        name,
+        phone,
+        password: hashedPassword,
+        role: countOfUsers? 'USER':'ADMIN'
+    });
 };
 
 exports.login = async (req, res) => {
