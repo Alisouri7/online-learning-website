b38d9d0c24bdb291dbe4e9d9b1e78ebc58733512
Ali Souri <souri7205@gmail.com>
2025-12-01 11:51:25 +0330

    implent registration mechanism

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const model = require('./../models/user');
+const userModel = require('./../models/user');
 
 const registerValidator = require('./../validators/register')
 
@@ -6,6 +6,15 @@ exports.register = async (req, res) => {
     const validationResult = registerValidator(req.body);
     if (!validationResult) {
         return res.status(422).json(validationResult);
+    }
+
+    const {username, name, email, password, phone} = req.body;
+
+    const isUserExist = await userModel.findOne({
+        $or: [{username}, {email}]
+    });
+    if (isUserExist) {
+        return res.status(409).json({message: 'userame or email ha used before!'})
     }
 };
 
