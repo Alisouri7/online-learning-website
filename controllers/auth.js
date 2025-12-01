5e39b28c2b3e9e79384e66d5bc925796629fc944
Ali Souri <souri7205@gmail.com>
2025-12-01 11:36:00 +0330

    implent registration mechanism

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,12 @@
 const model = require('./../models/user');
 
-exports.register = async (req, res) => {
+const registerValidator = require('./../validators/register')
 
+exports.register = async (req, res) => {
+    const validationResult = registerValidator(req.body);
+    if (!validationResult) {
+        return res.status(422).json(validationResult);
+    }
 };
 
 exports.login = async (req, res) => {
