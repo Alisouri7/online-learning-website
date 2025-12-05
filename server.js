179df85513c4a51bff21c0a69ad07330aacadcb0
Ali Souri <souri7205@gmail.com>
2025-12-05 16:59:20 +0330

    recieve token

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const app = require('./app')
 const mongoose = require('mongoose')
 require('dotenv').config()
-
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT;
 
 (async () => {
     await mongoose.connect(process.env.MONGO_URI)
     console.log('db connected');
 })()
+
+app.get('/', (req, res) => {
+    console.log('token: ', req.header('Authorization').split(' ')[1]);
+    console.log(jwt.verify(req.header('Authorization').split(' ')[1], process.env.JWT_SECRET));
+    
+    res.status(200).json({message: 'ok'})
+})
 
 app.listen(port, () => {
     console.log(`server run on port ${port}`);
