6d33522c3f368f6a6ca6c43144f820502dfaeb62
Ali Souri <souri7205@gmail.com>
2025-12-05 18:50:26 +0330

    implent user token auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,25 @@
 const userModel = require('./../models/user')
 const jwt = require('jsonwebtoken')
 
-module.exports = async (req, res, next) => {
-    const authHeader = req.headers.authorization.split(' ')
+module.exports = async (req, res, next) => {                         //this middleware determines that user has a token in req object or not 
+    const authHeader = req.headers.authorization.split(' ');
 
     if (authHeader.length !== 2) {
         return res.status(403).json({ message: 'please login and recieve access token ' })
-    }
+    };
 
-    const token = authHeader[1]
+    const token = authHeader[1];
 
     try {
 
-        const jwtPayload = jwt.verify(token, process.env.JWT_SECRET)
+        const jwtPayload = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await userModel.findById(jwtPayload.id)
+        const user = await userModel.findById(jwtPayload.id).lean();
+        Reflect.deleteProperty(user, 'password');
         
+        req.user = user;
+        
+        next()
     } catch (err) {
         return res.json(err)
     }
