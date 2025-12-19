0e9a488a04036e8da9d5ccf1acf7a73eee6f45ba
Ali Souri <souri7205@gmail.com>
2025-12-19 22:31:52 +0330

    implent category route and controller

--- controllers/category.js
+++ controllers/category.js
@@ -8,3 +8,11 @@ exports.create = async (req, res) => {
 exports.getAll = async (req, res) => {
 
 }
+
+exports.remove = async (req, res) => {
+
+}
+
+exports.update = async (req, res) => {
+
+}
--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const authMiddlware = require('./../middlewares/auth');
 const isAdminMiddleware = require('./../middlewares/isAdmin');
-const categoryController = require('./../category.js')
+const categoryController = require('./../controllers/category')
 
 const router = express.Router();
 
 router.route('/')
                 .post(authMiddlware, isAdminMiddleware, categoryController.create)
                 .get(categoryController.getAll);
+
+
+router.route('/:id')
+                    .put(authMiddlware, isAdminMiddleware, categoryController.update)
+                    .delete(authMiddlware, isAdminMiddleware, categoryController.remove);
