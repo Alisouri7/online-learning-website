cd6a01aa8c653df6ab9556ca5d21e5ca1f9d762a
Ali Souri <souri7205@gmail.com>
2025-12-20 08:34:11 +0330

    implent category validator and controller

--- validators/cateory.js
+++ validators/cateory.js
@@ -1,14 +1,14 @@
 const Validator = require('fastest-validator');
 const v = new Validator()
 
-const schema = v.schema({
+const schema = {
     title: {
         type: 'string'
     },
     href: {
         type: 'string'
     }
-})
+}
 
 const check = v.compile(schema)
 
