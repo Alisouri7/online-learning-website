6658fbb7c0337bb6fe19442b862d57dea0091cba
Ali Souri <souri7205@gmail.com>
2025-12-20 11:42:22 +0330

    implent category delte

--- controllers/category.js
+++ controllers/category.js
@@ -1,27 +1,27 @@
-
+const mongoose = require('mongoose');
 const categoryModel = require('./../models/cateory');
-const categoryValidator = require('./../validators/cateory')
+const categoryValidator = require('./../validators/cateory');
 
 exports.create = async (req, res) => {
-    const {title, href} = req.body;
+    const { title, href } = req.body;
     const validationResult = categoryValidator.check(req.body);
 
     if (!validationResult) {
-        return res.status(400).json({message: 'category information is not valid'})
+        return res.status(400).json({ message: 'category information is not valid' })
     };
 
-    const isCategoryExist = await categoryModel.find({
-        $or: [{title}, {href}]
+    const isCategoryExist = await categoryModel.findOne({
+        $or: [{ title }, { href }]
     })
     if (isCategoryExist) {
-        return res.status().json({message: 'there is a category with same name or href'})
+        return res.status(409).json({ message: 'there is a category with same name or href' })
     };
-    
+
     const category = await categoryModel.create({
         title,
         href
     });
-    return res.status(201).json(category)
+    return res.json(category)
 }
 
 exports.getAll = async (req, res) => {
@@ -30,7 +30,15 @@ exports.getAll = async (req, res) => {
 }
 
 exports.remove = async (req, res) => {
+    const isObjectIdValid = mongoose.Types.ObjectId.isValid(req.params.id)
 
+    if (!isObjectIdValid) {
+        return res.json({message: 'categorys id is not valid'})
+    };
+
+    const category = await categoryModel.findOneAndDelete({_id: req.params.id})
+
+    return res.status(200).json(category)
 }
 
 exports.update = async (req, res) => {
