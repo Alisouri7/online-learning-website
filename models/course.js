5360ba17652df2ce8671c16cac73a1283408403f
Ali Souri <souri7205@gmail.com>
2025-12-22 10:14:04 +0330

    implent course model

--- models/course.js
+++ models/course.js
@@ -6,14 +6,38 @@ const schema = mongoose.Schema({
         type: String,
         required: true
     },
+        description: {
+        type: String,
+        required: true
+    },
+        cover: {
+        type: String,
+        required: true
+    },
     price: {
         type: Number,
         required: true
     },
-    free: {
+        support: {
+        type: String,
+        required: true
+    },
+        href: {
+        type: String,
+        required: true
+    },
+        status: {
+        type: String,
+        required: true
+    },
+        discount: {
         type: Number,
         required: true
-    }
+    },
+        categoryID: {
+        type: mongoose.Types.ObjectId,
+        ref: "Category"
+    },
 } , { timestamps: true});
 
 const model = mongoose.model('Course', schema);
