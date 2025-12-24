dd834bf4e1d1a89abda35b858c08fcd65d916109
Ali Souri <souri7205@gmail.com>
2025-12-24 09:40:53 +0330

    implent session create router and controller

--- routes/course.js
+++ routes/course.js
@@ -5,6 +5,7 @@ const isAdminMiddleware = require('./../middlewares/isAdmin');
 const courseController = require('./../controllers/course');
 const multer = require('multer');
 const multerCoverStorage = require('../utils/coverUploader');
+const multerVideoStorage = require('./../utils/videoUploader');
 
 
 router.route('/')
@@ -12,6 +13,14 @@ router.route('/')
     isAdminMiddleware, 
     multer({ storage: multerCoverStorage, limits: { fileSize: 10000000 } }).single('cover'), 
     courseController.create);
+
+
+router.route('/:id/sessions').post(
+    authMiddleware,
+    isAdminMiddleware,
+    multer({ storage: multerVideoStorage, limits: { fileSize: 100000000 }}).single('video'),
+    courseController.createSession
+);
 
 
 module.exports = router;
