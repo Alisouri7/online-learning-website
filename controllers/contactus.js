399f05384d060902a15bff249f0e4aad08423c2b
Ali Souri <souri7205@gmail.com>
2026-02-19 10:41:36 +0330

    config node mailer to answer contact us

--- controllers/contactus.js
+++ controllers/contactus.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const contactusModel = require('./../models/contactus');
-
+const nodemailer = require('nodemailer');
 
 exports.create = async (req, res) => {
-    const {name, email, phone, body} = req.body;
+    const { name, email, phone, body } = req.body;
 
     const contact = await contactusModel.create({
         name,
@@ -25,16 +25,29 @@ exports.remove = async (req, res) => {
     const isValidObjectID = mongoose.Types.ObjectId.isValid(req.params.id);
 
     if (!isValidObjectID) {
-        return res.json({message: 'id is not valid'})
+        return res.json({ message: 'id is not valid' })
     }
-    const contact = await contactusModel.findOneAndDelete({_id: req.params.id});
+    const contact = await contactusModel.findOneAndDelete({ _id: req.params.id });
 
-        if(!contact){
-            return res.status(404).json({message: 'contact not found'})
-        }
-    return res.status(204).json({message: `this contact ${contact} has been deleted` })
+    if (!contact) {
+        return res.status(404).json({ message: 'contact not found' })
+    }
+    return res.status(204).json({ message: `this contact ${contact} has been deleted` })
 };
 
 exports.answer = async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "souri7202@gmail.com",
+            pass: process.env.APP_PASSWORD // The 16-character App Password
+        },
+    });
 
+    const mailOptions = {
+        from: "souri7202@gmail.com",
+        to: req.body.email,
+        subject: "Hello World",
+        text: req.body.answer,
+    };
 };
