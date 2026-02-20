c9a18b276a019c9b61ea2fba661f5104aa956f0b
Ali Souri <souri7205@gmail.com>
2026-02-20 12:17:16 +0330

    implent news letter

--- controllers/newsletter.js
+++ controllers/newsletter.js
@@ -2,9 +2,17 @@ const newsletterModel = require('./../models/newsletter');
 
 exports.getAll = async (req, res) {
     const newsletters = await newsletterModel.find({}).lean();
-    return res,json(newsletters)
+    return res, json(newsletters)
 }
 
 exports.create = async (req, res) {
-    
+    const { email } = req.body;
+    if (email.match(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm)) {
+        const newEmail = await newsletterModel.create({
+            email: email
+        })
+        return res.json(newEmail)
+    }
+
+     return res.json({message: 'email s not valid'})
 }
