1e595ca6a9ffe9cc1ab8c751fee1a1b3e6ddc854
Ali Souri <souri7205@gmail.com>
2026-02-20 12:21:53 +0330

    implent news letter

--- controllers/newsletter.js
+++ controllers/newsletter.js
@@ -2,7 +2,7 @@ const newsletterModel = require('./../models/newsletter');
 
 exports.getAll = async (req, res) => {
     const newsletters = await newsletterModel.find({}).lean();
-    return res, json(newsletters)
+    return res.json(newsletters)
 }
 
 exports.create = async (req, res) => {
