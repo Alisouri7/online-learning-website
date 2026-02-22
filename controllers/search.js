2d20077b3022d2be793d104381f694b6e302211d
Ali Souri <souri7205@gmail.com>
2026-02-22 15:02:54 +0330

    implent search n site

--- controllers/search.js
+++ controllers/search.js
@@ -3,7 +3,7 @@ const courseModel = require('./../models/course');
 exports.get = async (req, res) => {
     const {keyword} = req.params;
 
-    const results = await courseModel.find({name: {
+    const results = await courseModel.find({name: {          //we can use or flag to search in name and description
         $regex: '.*' + keyword + '.*'
     }})
     return res.json(results)
