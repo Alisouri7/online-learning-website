a25793a50d49536a08e3726c0f7231a27820f10a
Ali Souri <souri7205@gmail.com>
2026-02-23 14:32:53 +0330

    implent notif controller

---
+++ controllers/notification.js
@@ -0,0 +1,14 @@
+const notificationModel = require('./../models/notification');
+const mongoose = require('mongoose');
+
+exports.create = async (req, res) => {
+
+}
+
+exports.get = async (req, res) => {
+    
+}
+
+exports.see = async (req, res) => {
+    
+}
