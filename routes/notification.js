1476e6d6c8d6f53a5f217fb009dae7099f213464
Ali Souri <souri7205@gmail.com>
2026-02-23 13:03:50 +0330

    implent notification roter

--- routes/notification.js
+++ routes/notification.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware  = require('./../middlewares/auth');
+const isAdminMiddleware  = require('./../middlewares/isAdmin');
+const notificationController = require('./../controllers/notification');
 
-
+router.post('/', authMiddleware, isAdminMiddleware, notificationController.create);
+router.get('/:adminID', notificationController.get);
+router.put('/:id/see', authMiddleware, isAdminMiddleware, notificationController.see);
 
 module.exports = router;
