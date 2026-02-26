0ee84de47c1cd89c696afe430d31050682d89750
Ali Souri <souri7205@gmail.com>
2026-02-26 22:09:20 +0330

    implent notif

--- controllers/notification.js
+++ controllers/notification.js
@@ -2,12 +2,12 @@ const notificationModel = require('./../models/notification');
 const mongoose = require('mongoose');
 
 exports.create = async (req, res) => {
-    const {message, admin} = req.body;
+    const { message, admin } = req.body;
 
     const isAdminIDValid = mongoose.Types.ObjectId.isValid(admin);
 
     if (!isAdminIDValid) {
-        return res.json({message: 'admin id is not valid'})
+        return res.json({ message: 'admin id is not valid' })
     }
 
     const notification = await notificationModel.create({
@@ -18,16 +18,25 @@ exports.create = async (req, res) => {
 }
 
 exports.get = async (req, res) => {
-    
-    const {_id} = req.user;
 
-    const adminNotifications = await notificationModel.find({admin: _id});
+    const { _id } = req.user;
+
+    const adminNotifications = await notificationModel.find({ admin: _id });
 
     return res.json(adminNotifications)
 }
 
 exports.see = async (req, res) => {
-    
+    const { id } = req.params;
+
+    const isNotoficationIDValid = mongoose.Types.ObjectId.isValid(id);
+
+    if (!isNotoficationIDValid) {
+        return res.json({message: 'id is not valid'})
+    }
+
+    const notification = await notificationModel.findOneAndupdate({_id: id}, {see: 1});
+    return res.json(notification)
 }
 
 exports.getAll = async (req, res) => {
