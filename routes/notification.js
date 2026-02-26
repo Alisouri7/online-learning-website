b2e64492fa78379c654cf65617425abfa90f5e93
Ali Souri <souri7205@gmail.com>
2026-02-26 11:09:56 +0330

    implent newsetter

--- routes/notification.js
+++ routes/notification.js
@@ -8,7 +8,7 @@ router.route('/')
                 .post(authMiddleware, isAdminMiddleware, notificationController.create)
                 .get(authMiddleware, isAdminMiddleware, notificationController.getAll);
 
-router.get('/:admins', notificationController.get);
+router.get('/admin', notificationController.get);
 router.put('/:id/see', authMiddleware, isAdminMiddleware, notificationController.see);
 
 module.exports = router;
