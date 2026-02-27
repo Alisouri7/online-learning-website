ce08930f31a0b085104e5a468bcabeca47220d10
Ali Souri <souri7205@gmail.com>
2026-02-27 20:24:29 +0330

    implent off router

--- routes/off.js
+++ routes/off.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('./../middlewares/auth');
+const isAdminMiddleware = require('./../middlewares/isAdmin');
+const offController = require('./../controllers/off');
+const auth = require('./../middlewares/auth');
 
+router.route('/')
+                .get(authMiddleware, isAdminMiddleware,offController.getAll)
+                .post(authMiddleware, isAdminMiddleware, offController.create);
 
+router.route('/all').post(authMiddleware, isAdminMiddleware, offController.setOnAll);
 
+router.route('/:code').post(authMiddleware, offController.getOne);
 
-
+router.route('/:code').delete(authMiddleware,isAdminMiddleware, offController.remove);
 
 module.exports = router
