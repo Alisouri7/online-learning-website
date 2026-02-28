2384ca3f3e73ad8eb14fe6917aedc0b447d2d8ac
Ali Souri <souri7205@gmail.com>
2026-02-28 09:11:20 +0330

    implent off controller

---
+++ controllers/off.js
@@ -0,0 +1,9 @@
+const courseModel = require('./../models/course');
+
+exports.setOnAll = async (req, res) => {
+    const {discount} = req.body;
+
+    await courseModel.updateMany({discount});
+
+    return res.json({message: 'discount set on all courses successfully'})
+}
